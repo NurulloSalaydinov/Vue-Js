dee4949d5c017ee75673247e8561d7f12ea237b4
Nurullo <86796772+NurulloSalaydinov@users.noreply.github.com>
2022-04-29 21:27:04 +0300

    Some Changes

--- vuetify_dashboard/src/router/index.js
+++ vuetify_dashboard/src/router/index.js
@@ -1,22 +1,26 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Dashboard from '../views/Dashboard.vue'
+import Projects from '../views/Projects.vue'
+import Team from '../views/Team.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'dashboard',
+    component: Dashboard
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/projects',
+    name: 'projects',
+    component: Projects
+  },
+  {
+    path: '/team',
+    name: 'team',
+    component: Team,
   }
 ]
 
